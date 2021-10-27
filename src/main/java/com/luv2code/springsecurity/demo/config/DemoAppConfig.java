45553836b4b65676bb4718c96c8fc8e3178ccc55
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-27 16:16:55 +0700

    add authentication with jdbc by spring security

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,17 +1,33 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 
+	@Autowired
+	private Environment environment;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	// define a bean for ViewResolver
 
 	@Bean
@@ -23,6 +39,39 @@ public class DemoAppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	// define a bean for our security data source
+	
+	@Bean
+	public DataSource secuiryDataSource() {
+		
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		
+		try {
+			comboPooledDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		
+		comboPooledDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+		comboPooledDataSource.setUser(environment.getProperty("jdbc.user"));
+		comboPooledDataSource.setPassword(environment.getProperty("jdbc.password"));
+		
+		// set connection pool props
+		comboPooledDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		comboPooledDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		comboPooledDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		comboPooledDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return comboPooledDataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		
+		String propValueString = environment.getProperty(propName);
+		return Integer.parseInt(propValueString);
+		
 	}
 	
 }
--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,17 +16,26 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		// use jdbc authentication
+		// spring security handle data source from jdbc
+		
+		auth.jdbcAuthentication().dataSource(dataSource);
+		
+		
 		// add our users for in memory authentication
 		
-		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(userBuilder.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(userBuilder.username("fakie").password("test123").roles("EMPLOYEE", "MANAGER"))
-			.withUser(userBuilder.username("mi").password("test123").roles("EMPLOYEE", "ADMIN"));
+//		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser(userBuilder.username("john").password("test123").roles("EMPLOYEE"))
+//			.withUser(userBuilder.username("fakie").password("test123").roles("EMPLOYEE", "MANAGER"))
+//			.withUser(userBuilder.username("mi").password("test123").roles("EMPLOYEE", "ADMIN"));
 		
 	}
 
